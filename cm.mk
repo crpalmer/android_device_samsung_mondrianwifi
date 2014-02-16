16c3d112064bedaa6e29538bd70cafd51bfb014b
Christopher R. Palmer <crpalmer@gmail.com>
2014-02-16 05:58:01 -0500

    mondrianwifi: Kang the whole thing from hlte

---
+++ cm.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, device/samsung/hlte/full_hlte.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := hlte
+PRODUCT_NAME := cm_hlte
