aff098fc7afdcf99791437578d52c72aa222f635
Christopher R. Palmer <crpalmer@gmail.com>
2014-02-20 20:34:38 -0500

    mondrianwifi: Update build fingerprint/desc from US firmware
    Change-Id: Ieb7da2b78af1ba45bc7e76b3659506df17a2bc8b

--- cm.mk
+++ cm.mk
@@ -10,8 +10,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=mondrianwifi \
     TARGET_DEVICE=mondrianwifi \
-    BUILD_FINGERPRINT="samsung/mondrianwifixx/mondrianwifi:4.4.2/KOT49H/T320XXU1ANAI:user/release-keys" \
-    PRIVATE_BUILD_DESC="mondrianwifixx-user 4.4.2 KOT49H T320XXU1ANAI release-keys"
+    BUILD_FINGERPRINT="samsung/mondrianwifiue/mondrianwifiue:4.4.2/KOT49H/T320UEU1ANAI:user/release-keys" \
+    PRIVATE_BUILD_DESC="mondrianwifiue-user 4.4.2 KOT49H T320UEU1ANAI release-keys"
 
 PRODUCT_DEVICE := mondrianwifi
 PRODUCT_NAME := cm_mondrianwifi
