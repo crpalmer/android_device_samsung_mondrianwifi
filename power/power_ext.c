7926cb791ef8c7c4807e234d67dafb30588f09a8
Christopher R. Palmer <crpalmer@gmail.com>
2014-02-27 09:19:43 -0500

    mondrianwifi: power: Record the inv master toggle for sensors
    I'm not sure if we need this or not, so it doesn't do anything.
    
    Change-Id: If3071eb02bc1f35ddff9049dce82172a38e3876d

--- power/power_ext.c
+++ power/power_ext.c
@@ -24,7 +24,7 @@
 #define TOUCHKEY_POWER "/sys/class/input/input2/enabled"
 //#define SPEN_POWER "/sys/class/input/input3/enabled"
 #define TSP_POWER "/sys/class/input/input4/enabled"
-#define GPIO_KEYS_POWER "/sys/class/input/input21/enabled"
+//#define INV_POWER "/sys/bus/iio/devices/iio:device0/master_enable"
 
 static void sysfs_write(char *path, char *s) {
     char buf[80];
@@ -51,7 +51,7 @@ void cm_power_set_interactive_ext(int on) {
     sysfs_write(TSP_POWER, on ? "1" : "0");
     sysfs_write(TOUCHKEY_POWER, on ? "1" : "0");
     //sysfs_write(SPEN_POWER, on ? "1" : "0");
-    sysfs_write(GPIO_KEYS_POWER, on ? "1" : "0");
+    //sysfs_write(INV_POWER, on ? "1" : "0");
 }
 
 
