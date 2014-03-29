448097cf93277119e063f429d4034b7869af658e
Christopher R. Palmer <crpalmer@gmail.com>
2014-03-29 05:36:01 -0400

    mondrianwifi: Use the QCOM power HAL
    Change-Id: Ibc0d35f240faf62d6baa740c234b6a84813e3c20

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,7 +125,8 @@ MAX_EGL_CACHE_KEY_SIZE := 12*1024
 # of the device.
 MAX_EGL_CACHE_SIZE := 2048*1024
 
-# PowerHAL extension
+# PowerHAL
+TARGET_POWERHAL_VARIANT := qcom
 TARGET_POWERHAL_SET_INTERACTIVE_EXT := device/samsung/mondrianwifi/power/power_ext.c
 
 # The "new" GPS is really the old GPS, override it.
