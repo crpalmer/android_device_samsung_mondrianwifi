d168c2d8934dfeb485f434c30e394edc5697f69b
Christopher R. Palmer <crpalmer@gmail.com>
2014-05-29 05:30:44 -0400

    mondrianwifi: init: Configure plug_boost
    Change-Id: I5d7635ad73894bbef799f9fd4e09024454970769

--- full_mondrianwifi.mk
+++ full_mondrianwifi.mk
@@ -23,7 +23,7 @@
  
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := mondrianwifi
