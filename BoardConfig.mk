6beb5a97010784486499fed57533921c069a19f7
Christopher R. Palmer <crpalmer@gmail.com>
2014-07-25 17:46:31 -0400

    mondrianwifi: Switch to our power HAL
    Change-Id: I50cdc6de908076dde275258fb88e079877fe15f4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,7 +103,7 @@ MAX_EGL_CACHE_KEY_SIZE := 12*1024
 MAX_EGL_CACHE_SIZE := 2048*1024
 
 # PowerHAL
-#TARGET_POWERHAL_VARIANT := mondrianwifi
+TARGET_POWERHAL_VARIANT := mondrianwifi
 
 # The "new" GPS is really the old GPS, override it.
 BOARD_HAVE_NEW_QC_GPS :=
