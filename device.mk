8570bd30d21ac7dec5c9c7fb7403d2486d205ef7
Christopher R. Palmer <crpalmer@gmail.com>
2014-11-27 04:07:28 -0500

    mondrianwifi: camera: Product packages moved to here
    Change-Id: I05d02ba2bc83f7fe0e68d91a3542eb5ef27accb6

--- device.mk
+++ device.mk
@@ -48,6 +48,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8974 \
+    libxml2
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8974
