f5ceb9fda15fa46b7ff7fc2f96aad02514b3aac2
Christopher R. Palmer <crpalmer@gmail.com>
2015-05-05 13:50:47 -0400

    mondrianwifi: Fix AAPT config derp
    I accidentally broke this when fixing the PRODUCT_AAPT_CONFIG
    back in January.
    
    Change-Id: I494947153d5c18c18cdf51e7dad4a7a05c840ddd

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
