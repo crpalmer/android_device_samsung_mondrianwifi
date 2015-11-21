65f724c0cfcc745795528e36a860f81e30602122
Christopher R. Palmer <crpalmer@gmail.com>
2015-11-21 07:06:36 -0500

    mondrianwifi: Don't need SEPOLICY_UNION anymore
    Change-Id: I0d2aaa18500492449e3718a00660ca8b5de03e7c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,25 +100,6 @@ TARGET_RECOVERY_FSTAB := device/samsung/mondrianwifi/rootdir/etc/fstab.qcom
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/samsung/mondrianwifi/sepolicy
 
-BOARD_SEPOLICY_UNION += \
-  file_contexts \
-  file.te \
-  init.te \
-  keystore.te \
-  mediaserver.te \
-  mm-pp-daemon.te \
-  mm-qcamerad.te \
-  mpdecision.te \
-  rmt_storage.te \
-  system_app.te \
-  system_server.te \
-  tee.te \
-  thermal-engine.te \
-  time_daemon.te \
-  ueventd.te \
-  vold.te \
-  wcnss-service.te \
-
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAVE_SAMSUNG_WIFI := true
