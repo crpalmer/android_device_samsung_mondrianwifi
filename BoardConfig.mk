002461bda33c9d041349e4814c8a88f2ff91469e
Christopher R. Palmer <crpalmer@gmail.com>
2016-06-05 07:00:54 -0400

    mondrianwif: wlan: Remove unneeded SAMSUNG define
    This allow building of Samsung wifi hacks which we aren't using.
    
    Change-Id: I321df272f23071c60486374633a58743f62668e9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,7 +102,6 @@ BOARD_SEPOLICY_DIRS += device/samsung/mondrianwifi/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
-BOARD_HAVE_SAMSUNG_WIFI := true
 BOARD_WLAN_DEVICE := qcwcn
 BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
