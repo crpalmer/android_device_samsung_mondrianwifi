aa198f3ce087c80b871b6be365bad2fd7035a841
Josue Rivera <prbassplayer@gmail.com>
2016-09-06 02:19:45 +0200

    mondriawnifi: Set arm-linux-androideabi- as default toolchain prefix
    Change-Id: Ib498a7c9798af2d3c1abd7f83f9372ef11ee468c
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
 TARGET_KERNEL_CONFIG := cyanogenmod_mondrian_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/mondrianwifi
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # Audio
 BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
